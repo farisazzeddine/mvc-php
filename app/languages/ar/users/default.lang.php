31addb530f205d3ca3279306e9bc6bd73305318c
azdine <farisazzeddine3@gmail.com>
2020-09-20 12:55:04 +0100

    add parametere languages

--- app/languages/ar/users/default.lang.php
+++ app/languages/ar/users/default.lang.php
@@ -1,3 +1,13 @@
 <?php
 $_['app_title']=" امتياز المستخدمين";
 $_['text_us_header']=" المستخدمين و الصلاحيات";
+$_['text_new_item']=" مستخدم جديد";
+$_['text_table_user_usersName']="الإ سم";
+$_['text_table_user_password']="كلمة المرور";
+$_['text_table_user_email']="البريد الإلكتروني";
+$_['text_table_user_phoneNumber']="رقم الهاتف";
+$_['text_table_groupId']="مجموعة";
+$_['text_table_subscriptionDate']="تاريخ التسجيل";
+$_['text_table_last_login']="آخر تسجيل دخول";
+$_['text_table_Action']="التحكم";
+$_['text_delete_confirm']="اهل تريد حذف هذا الموظف؟";
--- app/languages/fr/users/default.lang.php
+++ app/languages/fr/users/default.lang.php
@@ -1,3 +1,13 @@
 <?php
-$_['app_title']=" utilisateurs et privilège ";
-$_['text_us_header']=" utilisateurs et privilège ";
+$_['app_title']=" Utilisateurs et privilège ";
+$_['text_us_header']=" Utilisateurs et privilège ";
+$_['text_new_item']="Nouveau Utilisateur";
+$_['text_table_user_usersName']="Nom";
+$_['text_table_user_password']="Password";
+$_['text_table_user_email']="E-mail";
+$_['text_table_user_phoneNumber']="N° de Telephone";
+$_['text_table_groupId']="Groupe";
+$_['text_table_subscriptionDate']="Date d'inscription";
+$_['text_table_last_login']="Dernière connexion";
+$_['text_table_Action']="Controle";
+$_['text_delete_confirm']="Voulez-vous supprimer cet employé?";
