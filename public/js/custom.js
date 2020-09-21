354f3bd2757a8bc45df22c86b426794e2142d006
azdine <farisazzeddine3@gmail.com>
2020-09-22 00:41:52 +0100

    ajoute des designe pour fieldset et labels

--- public/js/custom.js
+++ public/js/custom.js
@@ -19,3 +19,16 @@ $(document).ready( function () {
     $('#table_id').DataTable();
 } );
 
+// JavaScript for label effects only
+$(window).load(function(){
+    $(".col-3 input").val("");
+
+    $(".input-effect input").focusout(function(){
+        if($(this).val() != ""){
+            $(this).addClass("has-content");
+        }else{
+            $(this).removeClass("has-content");
+        }
+    })
+});
+
