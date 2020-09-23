3223f0ea7158a0541e0a9260edb47801bb510916
faris Az'din <farisazzeddine3@gmail.com>
2020-09-23 16:40:20 +0100

    users group folder with views

---
+++ app/views/usersgroups/create.view.php
@@ -0,0 +1,39 @@
+<div class="container" style="padding-top: 30px;">
+
+    <form method="post" enctype="application/x-www-form-urlencoded">
+        <fieldset>
+            <legend><?= $text_legend ?></legend>
+            <div class="row mt-3">
+                <div class="col-md-5 col-3 input-effect">
+                    <input class="effect-17" type="text" name="Group_name" id="Group_name"  placeholder="" maxlength="20">
+                    <label> <?= $text_label_group_title ?></label>
+                    <span class="focus-border"></span>
+                </div>
+            </div>
+            <div class="col-md-12 ml-4">
+                <label> <?= $text_label_privileges ?></label>
+                <div class=" d-block">
+                    <?php if(false !== $privileges  ) : foreach ($privileges as $privilege): ?>
+
+                        <input type="checkbox" class="form-check-input" name="privileges[]" id="exampleCheck<?=$privilege->Privilege_id ?>" value="<?=$privilege->Privilege_id ?>">
+                        <label class="form-check-label d-block" for="exampleCheck<?=$privilege->Privilege_id ?>"><?=$privilege->PrivilegeTitle ?></label>
+
+                    <?php  endforeach; else : ?>
+                        <label>  il ya pas des données  </label>
+                    <?php endif; ?>
+                </div>
+
+            </div>
+
+
+
+            <div class="col-md-12 form-group">
+                <button class="btn btn-teal float mb-2" type="submit" name="submit"><?= $text_label_save; ?>
+                </button>
+            </div>
+        </fieldset>
+
+    </form>
+
+
+</div>
--- app/views/usersgroups/default.view.php
+++ app/views/usersgroups/default.view.php
@@ -1,31 +1,24 @@
 <div class="container">
     <h4 class="bg-title"><?= $text_group_header ?></h4>
-    <a href="/groups/create" class="button"><i class="fa fa-plus"></i> <?= $text_new_item ?></a>
-
-    <div class="mt-3">
-        <div class="col-3 input-effect">
-            <input class="effect-17" type="text" placeholder="">
-            <label>First Name</label>
-            <span class="focus-border"></span>
-        </div>
-    </div>
+    <a href="/usersgroups/create" class="button"><i class="fa fa-plus"></i> <?= $text_new_item ?></a>
 
         <table id="table_id" class="table table-hover display">
         <thead>
         <tr>
             <th scope="col">#</th>
-            <th scope="col">                     <?= $text_table_group_groupName ?>          </th>
-            <th  class="text-center" scope="col"><?= $text_table_Action ?>                  </th>
+            <th scope="col">                     <?= $text_table_group_groupName ?>  </th>
+            <th class="text-center" scope="col">    <?= $text_table_Action ?>        </th>
         </tr>
         </thead>
         <tbody>
+        <?php $index = 0; ?>
         <?php if(false !== $groups  ) : foreach ($groups as $group): ?>
               <tr>
-            <th scope="row"><?= $group->Group_id  ?></th>
+            <th scope="row"><?=$index++  ?></th>
             <td><?= $group->Group_name  ?></td>
             <td class="text-center">
-                <a href="/user/edit/<?= $group->Group_id ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                <a href="/user/delete/<?= $group->User_id ?>"
+                <a href="/usersgroups/edit/<?= $group->Group_id ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                <a href="/usersgroups/delete/<?= $group->Group_id ?>"
                    onclick="if(!confirm('<?= $text_delete_confirm ?>')) return false;"><i class="fa fa-trash-o" aria-hidden="true"></i>
                 </a>
             </td>
