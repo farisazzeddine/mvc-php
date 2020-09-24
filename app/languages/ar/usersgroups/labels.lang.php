7457e182d19bd03b59efee57432c88f5a8e3630a
faris Az'din <farisazzeddine3@gmail.com>
2020-09-24 15:40:59 +0100

    modification des terme langue en usersgroups

--- app/languages/ar/usersgroups/labels.lang.php
+++ app/languages/ar/usersgroups/labels.lang.php
@@ -1,5 +1,5 @@
 <?php
 
 $_['text_label_group_title']=" أسم المجموعة  ";
-$_['text_label_privileges']=" صلاحيات تابعة لهذه المجموعة ";
+$_['text_label_privileges']=" صلاحيات تابعة لهذه المجموعة: ";
 $_['text_label_save']=" حفظ ";
--- app/languages/fr/usersgroups/labels.lang.php
+++ app/languages/fr/usersgroups/labels.lang.php
@@ -1,5 +1,5 @@
 <?php
 
 $_['text_label_group_title'] ="Nom de Groupe";
-$_['text_label_privileges']  ="Pouvoirs appartenant à ce groupe";
+$_['text_label_privileges']  ="Pouvoirs appartenant à ce groupe:";
 $_['text_label_save']="Enregistré";
